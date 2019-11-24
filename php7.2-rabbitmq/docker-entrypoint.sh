1aa59e61a81621fa52bf0cc751be239664902ad8
wangyaxian <wangyaxian@higohappy.com>
2019-11-24 10:38:50 +0800

    PHP

--- php7.2-rabbitmq/docker-entrypoint.sh
+++ php7.2-rabbitmq/docker-entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
+# @auther <1822581649@qq.com>
 
-# 定时任务
 /usr/sbin/init
 /usr/sbin/crond
-
 echo "info: starting php-fpm.."
-
-# 启动FPM
 /usr/sbin/php-fpm -c /etc/php.ini -y /etc/php-fpm.conf
-
 tail -f /dev/null
